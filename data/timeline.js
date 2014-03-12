32c1890edc28c8d23216c78e267c8d4778cd446e
Erik Vold <evold@mozilla.com>
2014-03-12 00:18:41 -0700

    the timeline is most recent to least recent now, and I'm using the full width of the thumnails for the moment

--- data/timeline.js
+++ data/timeline.js
@@ -1,6 +1,7 @@
 
 addon.port.on('new-history-item', function(details) {
   let div = document.createElement('div');
+  div.setAttribute('data-index', details.index);
   //div.innerHTML = details.title;
   //div.innerHTML = details.thumbnail;
   let img = document.createElement('img');
@@ -12,7 +13,20 @@ addon.port.on('new-history-item', function(details) {
   }, false);
 
   div.appendChild(img);
-  document.body.appendChild(div);
+  let lastEle;
+  for (let i = document.body.children.length - 1; i >=0; i--) {
+    let ele = document.body.children[i];
+    if ((ele.getAttribute('data-index')*1) > details.index) {
+      break;
+    }
+    lastEle = ele;
+  }
+  if (lastEle) {
+    document.body.insertBefore(div, lastEle);
+  }
+  else {
+    document.body.appendChild(div);
+  }
 });
 
 window.onload = function() {
